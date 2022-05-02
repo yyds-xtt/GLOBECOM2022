1a556ce054f197930d01212aa73c073545a75839
giang <phamhuonggiang.hust@gmail.com>
2022-05-02 17:24:00 +0900

    update plot figure

--- Plot_figure.py
+++ Plot_figure.py
@@ -14,7 +14,7 @@ def create_img_folder():
     print(f"Directory {os.getcwd()}")
     return path
 
-def plot_figure(): 
+def plot_scale_factor(): 
     csv_name = "V1.csv"
     # path = create_img_folder()
     print('create img foler')
@@ -57,3 +57,43 @@ def plot_figure():
 
 # plot_figure()
 
+def plot_delay():
+    csv_name = "V1.csv"
+    dth_arr = [1.0, 2.5, 3, 4]
+    delay = np.zeros_like(dth_arr)
+    user_energy = np.zeros_like(dth_arr)
+    uav_energy = np.zeros_like(dth_arr)
+    weighted_energy = np.zeros_like(dth_arr)
+
+    for idx, dthreshold in enumerate(dth_arr):
+        path = "{}/img/V ={:.2e},dth={:},lambda={:},T={:},W={:.2e},scale_delay={:}, psi = {:}, f_u ={:.2e}/".format(os.getcwd(),
+    V, dthreshold, lambda_param, T, W, scale_delay, psi, f_u_max)
+        
+        file = path + csv_name
+        data = pd.read_csv(file)
+        delay[idx] = np.mean(data.delay)
+        user_energy[idx] = np.mean(data.energy_user)*1000
+        uav_energy[idx] = np.mean(data.energy_uav)*1000
+        weighted_energy[idx] = np.mean(data.weighted_energy)*1000
+        
+    plt.plot(dth_arr, weighted_energy, label='Weighted energy')
+    plt.plot(dth_arr, uav_energy, label='UAV energy')
+    plt.plot(dth_arr, user_energy, label='User energy')
+
+    plt.ylabel('Energy Consumption (mJ)')
+    plt.xlabel('Delay threshold (TS)')
+    plt.grid()
+    plt.legend()
+    plt.savefig('energy_vs_dth.png')
+    plt.show()
+
+
+    plt.plot(dth_arr, delay, '-o', label = "Delay (mJ)")
+    plt.ylabel('Delay')
+    plt.xlabel('Delay threshold (TS)')
+    plt.grid()
+    plt.legend()
+    plt.savefig('delay_vs_dth.png')
+    plt.show()
+
+plot_delay()
